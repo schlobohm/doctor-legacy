082abff9a21fd295de6f26ca93e4a90709957e97
Nick Schlobohm <nks@schlobohm.one>
2022-03-21 11:08:11 +1000

    add: progress printouts

--- doctor.sh
+++ doctor.sh
@@ -24,30 +24,43 @@ mkdir meta; mkdir cron; mkdir appdata; mkdir media
 # meta reports
 cd "$OUTPUTDIR/meta"
 
+echo "running meta reports (date, path, system, hostname, ip)..."
+
 date > "date"
 echo "$0" > "path"
 uname -a > "system"
 hostname > "hostname"
 hostname -I > "ip"
 
+echo "finished meta reports"
 
 # cron reports
 cd "$OUTPUTDIR/cron"
 
+echo "running cron reports..."
+
 cp /etc/crontab .
 find /var/spool/cron/crontabs/ -type f -exec cp "{}" . \;
 
+echo "finished cron reports"
 
 # appdata reports
 cd "$OUTPUTDIR/appdata"
+
+echo "running appdata reports..."
 
 tree -alnsDF /mnt/appdata -o "files.tree"
 
 docker ps -a -s > "docker_ps_-a_-s.log"
 docker inspect $(docker ps -q) > "docker_inspect_(docker_ps_-q).log"
 
+echo "finished appdata reports"
 
 # media reports
 cd "$OUTPUTDIR/media"
 
+echo "running media reports..."
+
 tree -alnsDF -I "xrated" /mnt/media -o "files.tree"
+
+echo "finished media reports"
